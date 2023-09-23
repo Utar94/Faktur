f784601510e1286747eb69acac747f72f5cbdcf8
Francis Pion <francispion@hotmail.com>
2023-09-23 00:21:15 -0400

    Code Review.

--- backend/src/Logitar.Faktur.Domain/Extensions/FluentValidationExtensions.cs
+++ backend/src/Logitar.Faktur.Domain/Extensions/FluentValidationExtensions.cs
@@ -6,8 +6,6 @@ public static class FluentValidationExtensions
 {
   public static IRuleBuilderOptions<T, TProperty> WithPropertyName<T, TProperty>(this IRuleBuilderOptions<T, TProperty> options, string? propertyName)
   {
-    return propertyName == null ? options : options
-      .OverridePropertyName(propertyName)
-      .WithName(propertyName);
+    return propertyName == null ? options : options.OverridePropertyName(propertyName).WithName(propertyName);
   }
 }
