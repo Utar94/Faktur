fddad4277f3ae83ac043b4e5198ada1ebafcf150
Francis Pion <francis.pion@akinox.com>
2023-09-27 08:49:34 -0400

    Implemented Product controller.

---
+++ backend/src/Logitar.Faktur.Web/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Logitar.Faktur.Contracts;
+using Logitar.Faktur.Contracts.Products;
+using Logitar.Faktur.Contracts.Search;
+using Logitar.Faktur.Web.Extensions;
+using Logitar.Faktur.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Faktur.Web.Controllers;
+
+[ApiController]
+[Route("stores/{storeId}/products")]
+public class ProductController : ControllerBase
+{
+  private readonly IProductService productService;
+
+  public ProductController(IProductService productService)
+  {
+    this.productService = productService;
+  }
+
+  [HttpGet("{articleId}")]
+  public async Task<ActionResult<Product>> ReadAsync(string storeId, string articleId, CancellationToken cancellationToken)
+  {
+    Product? product = await productService.ReadAsync(storeId, articleId, cancellationToken);
+    return product == null ? NotFound() : Ok(product);
+  }
+
+  [HttpDelete("{articleId}")]
+  public async Task<ActionResult<AcceptedCommand>> RemoveAsync(string storeId, string articleId, CancellationToken cancellationToken)
+  {
+    return Accepted(await productService.RemoveAsync(storeId, articleId, cancellationToken));
+  }
+
+  [HttpPut("{articleId}")]
+  public async Task<ActionResult<AcceptedCommand>> SaveAsync(string storeId, string articleId,
+    [FromBody] SaveProductPayload payload, CancellationToken cancellationToken)
+  {
+    AcceptedCommand result = await productService.SaveAsync(storeId, articleId, payload, cancellationToken);
+    Uri uri = new($"{Request.GetBaseUrl()}/stores/{storeId}/products/{articleId}");
+
+    return Accepted(uri, result);
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<SearchResults<Product>>> SearchAsync(string storeId, [FromQuery] SearchProductsQuery query, CancellationToken cancellationToken)
+  {
+    return Ok(await productService.SearchAsync(query.ToPayload(storeId), cancellationToken));
+  }
+
+  [HttpPatch("{articleId}")]
+  public async Task<ActionResult<AcceptedCommand>> UpdateAsync(string storeId, string articleId,
+    [FromBody] UpdateProductPayload payload, CancellationToken cancellationToken)
+  {
+    return Accepted(await productService.UpdateAsync(storeId, articleId, payload, cancellationToken));
+  }
+}
---
+++ backend/src/Logitar.Faktur.Web/Models/SearchProductsQuery.cs
@@ -0,0 +1,31 @@
+﻿using Logitar.Faktur.Contracts.Products;
+using Logitar.Faktur.Contracts.Search;
+
+namespace Logitar.Faktur.Web.Models;
+
+public record SearchProductsQuery : SearchQuery
+{
+  public string? DepartmentNumber { get; set; }
+
+  public SearchProductsPayload ToPayload(string storeId)
+  {
+    SearchProductsPayload payload = new();
+
+    ApplyQuery(payload);
+
+    payload.StoreId = storeId;
+    payload.DepartmentNumber = DepartmentNumber;
+
+    List<SortOption> sort = ((SearchPayload)payload).Sort;
+    payload.Sort = new List<ProductSortOption>(sort.Capacity);
+    foreach (SortOption option in sort)
+    {
+      if (Enum.TryParse(option.Field, out ProductSort field))
+      {
+        payload.Sort.Add(new ProductSortOption(field, option.IsDescending));
+      }
+    }
+
+    return payload;
+  }
+}
