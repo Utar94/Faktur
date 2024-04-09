33c844cffc6ba88d5e0d824bac9b03889e07bbc0
Francis Pion <francispion@hotmail.com>
2024-04-09 12:18:43 -0400

    Installed dependencies.

---
+++ frontend/src/i18n/index.ts
@@ -0,0 +1,38 @@
+import { createI18n } from "vue-i18n";
+
+import en from "./en";
+import fr from "./fr";
+
+type MessageSchema = typeof fr;
+
+export default createI18n<[MessageSchema], "en" | "fr">({
+  legacy: false,
+  locale: "fr",
+  fallbackLocale: "fr",
+  messages: {
+    en,
+    fr,
+  },
+  datetimeFormats: {
+    en: {
+      medium: {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      },
+    },
+    fr: {
+      medium: {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      },
+    },
+  },
+});
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,26 @@
-import "./assets/main.css";
-
+import persistedState from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
+import fontAwesome from "./fontAwesome";
+import i18n from "./i18n";
 import router from "./router";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./assets/styles/main.css";
+// import "./validation"; // TODO(fpion): validation
 
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia();
+pinia.use(persistedState);
+
+app.use(fontAwesome);
+app.use(i18n);
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
