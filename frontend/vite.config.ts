b8ab0e98ec3412167a1f66aad654b45011db559a
Francis Pion <francispion@hotmail.com>
2024-04-09 11:51:43 -0400

    Created a base Vue3 project. (#94)
    * Created a base Vue3 project.
    
    * Added a frontend build.

---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import VueDevTools from "vite-plugin-vue-devtools";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
+import packageJson from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), VueDevTools()],
+  define: {
+    "import.meta.env.VITE_APP_VERSION": JSON.stringify(packageJson.version),
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 7789,
+  },
+});
