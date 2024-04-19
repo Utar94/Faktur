d7a78d37dae9848efd785ba68c75499260877ab4
Francis Pion <francispion@hotmail.com>
2024-04-19 00:50:56 -0400

    Revert CountSelect and SortSelect.

--- frontend/src/components/shared/CountSelect.vue
+++ frontend/src/components/shared/CountSelect.vue
@@ -1,35 +1,27 @@
 <script setup lang="ts">
-import { parsingUtils, type SelectOption } from "logitar-vue3-ui";
+import { TarSelect, parsingUtils, type SelectOption } from "logitar-vue3-ui";
 import { ref } from "vue";
-
-import AppSelect from "./AppSelect.vue";
+import { useI18n } from "vue-i18n";
 
 const { parseNumber } = parsingUtils;
+const { t } = useI18n();
 
 defineProps<{
   modelValue?: number;
 }>();
 
-const options = ref<SelectOption[]>([
-  { text: "10", value: "10" },
-  { text: "25", value: "25" },
-  { text: "50", value: "50" },
-  { text: "100", value: "100" },
-]);
+const options = ref<SelectOption[]>([{ text: "10" }, { text: "25" }, { text: "50" }, { text: "100" }]);
 
 defineEmits<{
   (e: "update:model-value", value?: number): void;
 }>();
-
-// TODO(fpion): always ignore validation
-// TODO(fpion): does not work properly without option values
 </script>
 
 <template>
-  <AppSelect
+  <TarSelect
     floating
     id="count"
-    label="count"
+    :label="t('count')"
     :model-value="modelValue?.toString()"
     :options="options"
     @update:model-value="$emit('update:model-value', parseNumber($event))"
--- frontend/src/components/shared/SortSelect.vue
+++ frontend/src/components/shared/SortSelect.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import { TarCheckbox, type SelectOption } from "logitar-vue3-ui";
+import { TarCheckbox, TarSelect, type SelectOption } from "logitar-vue3-ui";
 import { useI18n } from "vue-i18n";
-
-import AppSelect from "./AppSelect.vue";
 
 const { t } = useI18n();
 
@@ -22,23 +20,20 @@ defineEmits<{
   (e: "descending", value: boolean): void;
   (e: "update:model-value", value?: string): void;
 }>();
-
-// TODO(fpion): always ignore validation
-// TODO(fpion): sort resets when descending changes
 </script>
 
 <template>
-  <AppSelect
+  <TarSelect
     floating
     :id="id"
-    label="sort.select.label"
+    :label="t('sort.select.label')"
     :model-value="modelValue"
     :options="options"
-    placeholder="sort.select.placeholder"
+    :placeholder="t('sort.select.placeholder')"
     @update:model-value="$emit('update:model-value', $event)"
   >
     <template #after>
       <TarCheckbox :id="`${id}_desc`" :label="t('sort.isDescending')" :model-value="descending" @update:model-value="$emit('descending', $event)" />
     </template>
-  </AppSelect>
+  </TarSelect>
 </template>
