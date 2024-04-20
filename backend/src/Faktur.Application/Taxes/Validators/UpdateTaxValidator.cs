3604d1639bb6f029d1b7870090891d56c8c135ae
Francis Pion <francispion@hotmail.com>
2024-04-20 15:48:13 -0400

    Refactored tax rate. (#147)
    * Refactored tax rate.
    
    * Implemented tax rate validation.

--- backend/src/Faktur.Application/Taxes/Validators/UpdateTaxValidator.cs
+++ backend/src/Faktur.Application/Taxes/Validators/UpdateTaxValidator.cs
@@ -10,7 +10,7 @@ internal class UpdateTaxValidator : AbstractValidator<UpdateTaxPayload>
   public UpdateTaxValidator()
   {
     When(x => !string.IsNullOrWhiteSpace(x.Code), () => RuleFor(x => x.Code!).SetValidator(new TaxCodeValidator()));
-    When(x => x.Rate.HasValue, () => RuleFor(x => x.Rate!.Value).GreaterThan(0.0));
+    When(x => x.Rate.HasValue, () => RuleFor(x => x.Rate!.Value).SetValidator(new TaxRateValidator()));
 
     When(x => !string.IsNullOrWhiteSpace(x.Flags?.Value), () => RuleFor(x => x.Flags!.Value!).SetValidator(new FlagsValidator()));
   }
---
+++ backend/src/Faktur.Domain/Taxes/TaxRateValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Faktur.Domain.Taxes;
+
+public class TaxRateValidator : AbstractValidator<double>
+{
+  public TaxRateValidator(string? propertyName = null)
+  {
+    RuleFor(x => x).GreaterThan(0).LessThan(1).WithPropertyName(propertyName);
+  }
+}
